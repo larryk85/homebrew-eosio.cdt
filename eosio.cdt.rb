e2032f301641dfb014c2ef2ec8e9943cd57fc9c4
Bucky Kittinger <larrykittinger@gmail.com>
2018-09-26 11:08:07 -0400

    Update eosio.cdt.rb

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -4,23 +4,28 @@ class EosioCdt < Formula
    revision 0
    url "https://github.com/larryk85/eosio.cdt/archive/v1.2.1.tar.gz"
    version "1.2.1"
-   #sha1 yada yada
    
    option :universal
 
    depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
    depends_on :xcode => :build
-   depends_on :macos => :leopard
+   depends_on :macos => :high_sierra
    depends_on :arch =>  :intel
-   
+  
    bottle do
       root_url "https://github.com/larryk85/test/releases/download/1.0.5"
-      #cellar :any_skip_relocation
-      sha256 "95b9516ebd4d907fe9541b8700773dfa97bb18f1a5bc330ad0677e7b3d5b34a5" => :high_sierra
+      sha256 "4c1e8a59da8950d04226eaf32b775fefeae8570c1172fa7ed4aeccb6e46a0a33" => :high_sierra
    end
    def install
-      system "./build.sh"
-      system "./install.sh"
+      raise Error, only supporting binary packages at this time
    end
 end
 __END__
