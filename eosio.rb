2ea6f849224364461770cbaf29fa300fbb7133d1
Bucky Kittinger <larrykittinger@gmail.com>
2018-10-15 15:41:20 -0400

    Add files via upload

---
+++ eosio.rb
@@ -0,0 +1,31 @@
+class Eosio < Formula
+
+   homepage "https://github.com/eosio/eos"
+   revision 0
+   url "https://github.com/eosio/eos/archive/v1.3.2.tar.gz"
+   version "1.3.2"
+   
+   option :universal
+
+   depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
+   depends_on :xcode => :build
+   depends_on :macos => :high_sierra
+   depends_on :arch =>  :intel
+  
+   bottle do
+      root_url "https://github.com/eosio/eos/releases/download/v1.3.2"
+      sha256 "ec97ccf63276aff50624752de8cf4accd0206e781be02e7066e9a478c7f90e3a" => :high_sierra
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
+end
+__END__
