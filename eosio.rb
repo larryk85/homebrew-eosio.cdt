e8c289ae431bcca794aea1bcdfb0534a32338033
Bucky Kittinger <larrykittinger@gmail.com>
2018-10-16 10:15:05 -0400

    Update eosio.rb

--- eosio.rb
+++ eosio.rb
@@ -7,22 +7,17 @@ class Eosio < Formula
    
    option :universal
 
-   depends_on "cmake" => :build
-   depends_on "automake" => :build
-   depends_on "libtool" => :build
-   depends_on "wget" => :build
-   depends_on "gmp" => :build
-   depends_on "gettext" => :build
-   depends_on "doxygen" => :build
-   depends_on "graphviz" => :build
-   depends_on "lcov" => :build
-   depends_on :xcode => :build
+   depends_on "gmp" 
+   depends_on "gettext"
+   depends_on "openssl"
+   depends_on "gmp"
+   depends_on :xcode
    depends_on :macos => :high_sierra
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/larryk85/test/releases/download/1.1.7"
-      sha256 "ec97ccf63276aff50624752de8cf4accd0206e781be02e7066e9a478c7f90e3a" => :high_sierra
+      root_url "https://github.com/eosio/eos/releases/download/v1.3.2"
+      sha256 "529087a3c8ee040f588432ef4deca43c8ed176a572894eb14be6a7e3b76c0235" => :high_sierra
    end
    def install
       raise "Error, only supporting binary packages at this time"
