07ea6775e4aef43e20be1f2e5dfc207cf56ecaff
Bucky Kittinger <larrykittinger@gmail.com>
2018-10-17 10:44:20 -0400

    Update eosio.rb

--- eosio.rb
+++ eosio.rb
@@ -3,7 +3,7 @@ class Eosio < Formula
    homepage "https://github.com/eosio/eos"
    revision 0
    url "https://github.com/eosio/eos/archive/v1.3.2.tar.gz"
-   version "1.3.2"
+   version "1.4.0"
    
    option :universal
 
