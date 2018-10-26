461dfe9dff73445c38a7fd97477bff55c10521cf
Bucky Kittinger <larrykittinger@gmail.com>
2018-10-26 16:44:39 -0400

    Update and rename eosio.cdt.rb2 to eosio.cdt.rb

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -1,9 +1,9 @@
-class Eosio2Cdt < Formula
+class EosioCdt < Formula
 
    homepage "https://github.com/eosio/eosio.cdt"
    revision 0
-   url "https://github.com/eosio/eosio.cdt/archive/v1.3.1.tar.gz"
-   version "1.3.1"
+   url "https://github.com/eosio/eosio.cdt/archive/v1.3.2.tar.gz"
+   version "1.3.2"
    
    option :universal
 
@@ -21,9 +21,8 @@ class Eosio2Cdt < Formula
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.3.1"
-      sha256 "b2b2c916d7b6856b5e953b55a61834f9dd00c6c8daa9898b061e93eb4b32f70a" => :high_sierra
-      sha256 "d153a24a46e560e411bb20b1a848ca81218f9fdfa7faec8d9ab1c2c22bda864a" => :mojave
+      root_url "https://github.com/larryk85/test/releases/download/v1.3.2"
+      sha256 "16890693b4c6ab0c9a8aec28025c3830094f00734a3c36e386cfa40a496bfb73" => :high_sierra
    end
    def install
       raise "Error, only supporting binary packages at this time"
