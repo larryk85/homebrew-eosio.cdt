63ecbb4fbc08d2ea19b492d8df0e736686992c6c
Bucky Kittinger <larrykittinger@gmail.com>
2019-02-01 12:37:45 -0500

    Update eosio.cdt.rb

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -3,7 +3,7 @@ class EosioCdt < Formula
    homepage "https://github.com/eosio/eosio.cdt"
    revision 0
    url "https://github.com/eosio/eosio.cdt/archive/v1.5.0.tar.gz"
-   version "1.5.0-rc2"
+   version "1.6.0"
    
    option :universal
 
@@ -12,8 +12,8 @@ class EosioCdt < Formula
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/larryk85/test/releases/download/1.5.0"
-      sha256 "426a46a3036cc3d04e39eb24942c2d059b957a96df4dcb3c7cd847a98cd2efbe" => :high_sierra
+      root_url "https://github.com/larryk85/test/releases/download/1.6.0"
+      sha256 "1b8d1e7b727c115b796816ee8924b6f64b9dce0e070bec2505456a80fce3a2bb"=> :high_sierra
    end
    def install
       raise "Error, only supporting binary packages at this time"
