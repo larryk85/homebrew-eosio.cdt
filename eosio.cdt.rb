32a4d5046d485e964fa970634e1b72a37c12e0ae
Bucky Kittinger <larrykittinger@gmail.com>
2019-07-08 10:49:38 -0400

    Update eosio.cdt.rb

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -2,18 +2,27 @@ class EosioCdt < Formula
 
    homepage "https://github.com/eosio/eosio.cdt"
    revision 0
-   url "https://github.com/eosio/eosio.cdt/archive/v1.5.0.tar.gz"
-   version "1.6.0-rc1"
+   url "https://github.com/eosio/eosio.cdt/archive/v1.6.1.tar.gz"
+   version "1.6.1"
    
    option :universal
 
+   depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
    depends_on :xcode => :build
    depends_on :macos => :high_sierra
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/larryk85/test/releases/download/1.7.0"
-      sha256 "3697c2fde704bdd4628b40a1c6ace11e867431217aeabda7ff4631a62dc18030" => :high_sierra
+      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.6.1"
+      sha256 "c5e8159892745fa81b75abcc86e630be98bf42ea5d38adc7f8b8e6f4530c01e2" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
