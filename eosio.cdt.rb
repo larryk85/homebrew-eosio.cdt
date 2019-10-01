65ec85de52042c76cd06b76b1d93584946de5bc5
Bucky Kittinger <larrykittinger@gmail.com>
2019-10-01 18:02:55 -0400

    Update eosio.cdt.rb

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -3,7 +3,7 @@ class EosioCdt < Formula
    homepage "https://github.com/eosio/eosio.cdt"
    revision 0
    url "https://github.com/eosio/eosio.cdt/archive/v1.6.1.tar.gz"
-   version "1.7.0"
+   version "1.7.0-develop"
    
    option :universal
 
