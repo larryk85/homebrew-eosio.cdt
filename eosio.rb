ae6644e60785175274aac4d8331e062616f9e16c
Bucky Kittinger <larrykittinger@gmail.com>
2019-11-01 16:31:13 -0400

    Update eosio.rb

--- eosio.rb
+++ eosio.rb
@@ -15,7 +15,7 @@ class Eosio < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/larryk85/test/releases/download/2.0.0/eosio-2.0.0-rc1.mojave.bottle.tar.gz"
+      root_url "https://github.com/larryk85/test/releases/download/2.0.0/"
       sha256 "b886ea0973df22836a26a743b1301de5af1688a61ce98549dd18ae911a460775" => :mojave
    end
    def install
